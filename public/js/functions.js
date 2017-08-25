5a4fd5a461cf790567796cc86117993568ddd5c9
dwoldenberg1 <dwoldenberg1@gmail.com>
2017-08-24 22:05:32 -0400

    misc styling stuff I forgot to commit

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,6 +5,7 @@ $(function(){
   /*** Title resizer ***/
   $(window).resize(function(){
     resizeTitle();
+    resizeAbout();
   });
 
 });
@@ -16,18 +17,29 @@ function resizeTitle(){
   if(tWidth > wWidth){
     while(tWidth > wWidth){
       t.css("font-size", Number(t.css("font-size").split("px")[0]) - 2 + "px");
+      var tSize = Number(t.css("font-size").split("px")[0]);
       tWidth = $(t.children()[0]).width();
+      if(tSize <= 50){
+        $('main').css("zoom", "90%").css("padding-left", "60px").addClass("morphed");
+      }
     }
   } else if(tWidth  < wWidth){
     while(tWidth < wWidth){
       t.css("font-size", Number(t.css("font-size").split("px")[0]) + 2 + "px");
       var tSize = Number(t.css("font-size").split("px")[0]);
       tWidth = $(t.children()[0]).width();
+      if(tSize >= 50 && $('main').hasClass("morphed")){
+        $('main').css("zoom", "unset").css("padding-left", "unset").removeClass("morphed");
+      }
       if(tSize > 80){
         t.css("font-size", "80px");
         return;
-      }
+      } 
     }
     t.css("font-size", (Number(t.css("font-size").split("px")[0]) - 4) + "px");
   }
+}
+
+function resizeAbout() {
+  var ww = 1;
 }
